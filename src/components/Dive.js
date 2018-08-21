80ce808988545cba882dd1946866a25792c8e313
AntonBrito <tonybrito85@gmail.com>
2018-08-21 16:39:27 -0400

    Created Dive component and show log name.

--- src/components/Dive.js
+++ src/components/Dive.js
@@ -1,8 +1,26 @@
 import React, { Component } from "react";
+// import PropTypes from "prop-types";
 
 class Dive extends Component {
   render() {
-    return <div />;
+    // const { name, divenumber, location, date, divebuddy, notes}
+    const { name } = this.props;
+    return (
+      <div className="card card-body mb-3">
+        <h4>
+          {name}
+          <i
+            onClick={() =>
+              this.setState({
+                showContactInfo: !this.state.showContactInfo
+              })
+            }
+            className="fas fa-sort-down"
+            style={{ cursor: "pointer" }}
+          />
+        </h4>
+      </div>
+    );
   }
 }
 
