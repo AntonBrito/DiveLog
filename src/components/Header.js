23173a6650ba0fd30d36e8e14e95356e56b997fb
AntonBrito <tonybrito85@gmail.com>
2018-08-21 16:36:26 -0400

    Created and set it up the header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,11 @@ import React from "react";
 
 const Header = props => {
   const { branding } = props;
-  return <div />;
+  return (
+    <div>
+      <h1>DiveLog</h1>
+    </div>
+  );
 };
 
 export default Header;
