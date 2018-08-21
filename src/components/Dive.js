7bc46ed59ac55f07f783e4d49818fda723a7c3a5
AntonBrito <tonybrito85@gmail.com>
2018-08-21 17:56:58 -0400

    Added propTypes. passing props and fields required.

--- src/components/Dive.js
+++ src/components/Dive.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
-// import PropTypes from "prop-types";
+import PropTypes from "prop-types";
 
 class Dive extends Component {
+  state = {
+    showLogInfo: false
+  };
   render() {
-    // const { name, divenumber, location, date, divebuddy, notes}
-    const { name } = this.props;
+    const { name, divenumber, location, date, divebuddy, notes } = this.props;
+    const { showLogInfo } = this.state;
     return (
       <div className="card card-body mb-3">
         <h4>
@@ -12,19 +15,34 @@ class Dive extends Component {
           <i
             onClick={() =>
               this.setState({
-                showContactInfo: !this.state.showContactInfo
+                showLogInfo: !this.state.showLogInfo
               })
             }
             className="fas fa-sort-down"
             style={{ cursor: "pointer" }}
           />
         </h4>
-        {/* {showLogInfo ?} (
-
-        ) */}
+        {showLogInfo ? (
+          <ul className="list-group">
+            <li className="list-group-item">Divenumber: {divenumber}</li>
+            <li className="list-group-item">Location: {location}</li>
+            <li className="list-gorup-item">Date: {date}</li>
+            <li className="list-gorup-item">Divebuddy: {divebuddy}</li>
+            <li className="list-gorup-item">Notes: {notes}</li>
+          </ul>
+        ) : null}
       </div>
     );
   }
 }
+
+Dive.propTypes = {
+  name: PropTypes.string.isRequired,
+  divenumber: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  divebuddy: PropTypes.string.isRequired,
+  notes: PropTypes.string.isRequired
+};
 
 export default Dive;
