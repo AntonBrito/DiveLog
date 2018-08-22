8480bdf2022559c9f016cfd4df9ea939b99efb91
AntonBrito <tonybrito85@gmail.com>
2018-08-22 14:53:15 -0400

    Added provider , holds state. Organized the sturcture of aplication

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import React, { Component } from "react";
 import "./App.css";
 import Dives from "./components/Dives";
 import Header from "./components/Header";
+
+import { Provider } from "./context";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header branding="DiveLog" />
-        <div className="container">
-          <Dives />
+      <Provider>
+        <div className="App">
+          <Header branding="DiveLog" />
+          <div className="container">
+            <Dives />
+          </div>
         </div>
-      </div>
+      </Provider>
     );
   }
 }
