bbb7657e88a890c13cd10bedcf2702444293a9e9
AntonBrito <tonybrito85@gmail.com>
2018-08-22 11:54:29 -0400

    Cleaned and organize app component structure

--- src/App.js
+++ src/App.js
@@ -1,31 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
-import Dive from "./components/Dive";
+import Dives from "./components/Dives";
 import Header from "./components/Header";
-
 import "bootstrap/dist/css/bootstrap.min.css";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header branding="DiveLog" />
         <div className="container">
-          <Dive
-            name="Anton Brito"
-            divenumber="021345"
-            location="Jamestown"
-            date="jun-22-2018"
-            divebuddy="RIley"
-            notes="We saw a shark and allot of start fish"
-          />
-          <Dive
-            name="Riley Brito"
-            divenumber="021345"
-            location="Jamestown"
-            date="jun-22-2018"
-            divebuddy="RIley"
-            notes="We saw a shark and allot of start fish"
-          />
+          <Dives />
         </div>
       </div>
     );
