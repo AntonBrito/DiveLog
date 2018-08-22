0738e8c16425211622f4a35f0fd3c13fda2b424e
AntonBrito <tonybrito85@gmail.com>
2018-08-22 11:53:33 -0400

    Created a higher component. Added state.

---
+++ src/components/Dives.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import Dive from "./Dive";
+
+class Dives extends Component {
+  state = {
+    dives: [
+      {
+        id: 1,
+        name: "Anton Brito",
+        location: "Jamestown",
+        date: "May-22-2018",
+        divebuddy: "John",
+        notes: "We saw a shark and allot of start fish"
+      },
+      {
+        id: 2,
+        name: "Riley Brito",
+        location: "Bahamas",
+        date: "feb-12-2018",
+        divebuddy: "Jess",
+        notes: "It was amazing"
+      },
+      {
+        id: 3,
+        name: "Anton Brito",
+        location: "Bahamas",
+        date: "feb-12-2018",
+        divebuddy: "Riley",
+        notes: "I cant wait to go back"
+      }
+    ]
+  };
+
+  render() {
+    const { dives } = this.state;
+    return (
+      <div>
+        {dives.map(dive => (
+          <Dive key={dive.id} dive={dive} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Dives;
