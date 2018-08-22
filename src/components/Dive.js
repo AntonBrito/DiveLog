9e8be0763b2461c35a12d6fcebd02005af4ca281
AntonBrito <tonybrito85@gmail.com>
2018-08-22 13:44:56 -0400

    Organize code structure

--- src/components/Dive.js
+++ src/components/Dive.js
@@ -3,8 +3,9 @@ import PropTypes from "prop-types";
 
 class Dive extends Component {
   state = {
-    showDiveInfo: true
+    showDiveInfo: false
   };
+
   render() {
     const { name, location, date, divebuddy, notes } = this.props.dive;
     const { showDiveInfo } = this.state;
@@ -22,6 +23,7 @@ class Dive extends Component {
             style={{ cursor: "pointer" }}
           />
         </h4>
+
         {showDiveInfo ? (
           <ul className="list-group">
             <li className="list-group-item">Location: {location}</li>
@@ -37,11 +39,6 @@ class Dive extends Component {
 
 Dive.propTypes = {
   dive: PropTypes.object.isRequired
-  // name: PropTypes.string.isRequired,
-  // location: PropTypes.string.isRequired,
-  // date: PropTypes.string.isRequired,
-  // divebuddy: PropTypes.string.isRequired,
-  // notes: PropTypes.string.isRequired
 };
 
 export default Dive;
