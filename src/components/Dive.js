135a2fa72fecb5794f16228c4a192a6660c1c134
AntonBrito <tonybrito85@gmail.com>
2018-08-22 11:56:41 -0400

    Added map. Cleaned some extra code.

--- src/components/Dive.js
+++ src/components/Dive.js
@@ -6,7 +6,7 @@ class Dive extends Component {
     showDiveInfo: true
   };
   render() {
-    const { name, divenumber, location, date, divebuddy, notes } = this.props;
+    const { name, location, date, divebuddy, notes } = this.props.dive;
     const { showDiveInfo } = this.state;
     return (
       <div className="card card-body mb-3">
@@ -24,11 +24,10 @@ class Dive extends Component {
         </h4>
         {showDiveInfo ? (
           <ul className="list-group">
-            <li className="list-group-item">Divenumber: {divenumber}</li>
             <li className="list-group-item">Location: {location}</li>
-            <li className="list-gorup-item">Date: {date}</li>
-            <li className="list-gorup-item">Divebuddy: {divebuddy}</li>
-            <li className="list-gorup-item">Notes: {notes}</li>
+            <li className="list-group-item">Date: {date}</li>
+            <li className="list-group-item">Divebuddy: {divebuddy}</li>
+            <li className="list-group-item">Notes: {notes}</li>
           </ul>
         ) : null}
       </div>
@@ -37,12 +36,12 @@ class Dive extends Component {
 }
 
 Dive.propTypes = {
-  name: PropTypes.string.isRequired,
-  divenumber: PropTypes.string.isRequired,
-  location: PropTypes.string.isRequired,
-  date: PropTypes.string.isRequired,
-  divebuddy: PropTypes.string.isRequired,
-  notes: PropTypes.string.isRequired
+  dive: PropTypes.object.isRequired
+  // name: PropTypes.string.isRequired,
+  // location: PropTypes.string.isRequired,
+  // date: PropTypes.string.isRequired,
+  // divebuddy: PropTypes.string.isRequired,
+  // notes: PropTypes.string.isRequired
 };
 
 export default Dive;
