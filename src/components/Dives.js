7ac873a067d7dd1de6938d0fbc93327736ffc97b
AntonBrito <tonybrito85@gmail.com>
2018-08-23 13:01:27 -0400

    Added a context reducer for actions

--- src/components/Dives.js
+++ src/components/Dives.js
@@ -3,16 +3,6 @@ import Dive from "./Dive";
 import { Consumer } from "../context";
 
 class Dives extends Component {
-  deleteDive = id => {
-    const { dives } = this.state;
-
-    const newDives = dives.filter(dive => dive.id !== id);
-
-    this.setState({
-      dives: newDives
-    });
-  };
-
   render() {
     return (
       <Consumer>
@@ -21,11 +11,7 @@ class Dives extends Component {
           return (
             <React.Fragment>
               {value.dives.map(dive => (
-                <Dive
-                  key={dive.id}
-                  dive={dive}
-                  deleteClickHandler={this.deleteDive.bind(this, dive.id)}
-                />
+                <Dive key={dive.id} dive={dive} />
               ))}
             </React.Fragment>
           );
