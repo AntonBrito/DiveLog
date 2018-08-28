4636724037e581ad715524bb0439a888c65ff617
AntonBrito <tonybrito85@gmail.com>
2018-08-28 13:03:17 -0400

    Added about component,  for a static page

---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default () => {
+  return (
+    <div>
+      <h1 className="display-4">About DiveLog</h1>
+      <p className="lead">Simple app top manage dives</p>
+      <p className="text-secondary">Version 1.0.0</p>
+    </div>
+  );
+};
