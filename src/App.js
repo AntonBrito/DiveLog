ce23585f0c751c077e650e4ce7130fd5e92290e3
AntonBrito <tonybrito85@gmail.com>
2018-08-30 23:14:38 -0400

    Edit Component. fetch Dive

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Dives from "./components/logs/Dives";
 import AddDive from "./components/logs/AddDive";
+import EditDive from "./components/logs/EditDive";
 import Header from "./components/layout/Header";
 import About from "./components/pages/About";
 import NotFound from "./components/pages/NotFound";
@@ -23,6 +24,7 @@ class App extends Component {
               <Switch>
                 <Route exact path="/" component={Dives} />
                 <Route exact path="/dive/add" component={AddDive} />
+                <Route exact path="/dive/edit:id" component={EditDive} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/test" component={Test} />
                 <Route component={NotFound} />
